40dece9013fa48d912a30b1557f772a4f811eef9
kotavr69 <144414802+kotavr69@users.noreply.github.com>
2023-10-17 23:00:00 +0700

    Добавьте файлы проекта.

---
+++ lab4.2.2/lab4.2.2.cpp
@@ -0,0 +1,24 @@
+﻿/*Вариант 11
+Дано целое число N (> 0).
+Найти сумму 2 + (N + 1)2 + (N + 2)2 + . . .
++ (2·N )2 (целое число).
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    int number;
+    int pos = 0;
+    cin >> number;
+    if (number > 0) {
+        int const NUMBER_CONST = number;
+        for (int i = 1; i < (NUMBER_CONST + 1); i++) {
+            pos += (number + i) * 2;
+        }
+        cout << "Сумма последовательности: " << 2 + pos << endl;
+    }
+    else {
+        cout << "Ошибка! Введите число больше 0!";
+    }
+}
